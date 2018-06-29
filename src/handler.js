b80fb0719138218d35ab265bdedbb84c8525561e
Pablo Cantero <pablohstc@gmail.com>
2018-06-29 12:46:44 -0400

    Reports no matches for the day

--- src/handler.js
+++ src/handler.js
@@ -4,6 +4,11 @@ const processMsgs = require('./history')
 
 module.exports.matches = async (event, context) => {
   const msgs = await processMatches()
+
+  if (msgs.length === 0) {
+    return ':sleeping: there are no matches today'
+  }
+
   const uniqueMsgs = await processMsgs(msgs)
   if (uniqueMsgs.length > 0) {
     const joinedMsgs = uniqueMsgs.join('\n\n')
