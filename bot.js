1558874b42ac5a1943c80b353304b68fc51c1546
Pablo Cantero <pablohstc@gmail.com>
2018-06-28 20:10:03 -0400

    Make bot name configurable

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const { WebClient } = require('@slack/client')
 
 const SLACK_TOKEN = process.env.SLACK_TOKEN
 const SLACK_CHANNEL = process.env.SLACK_CHANNEL
-const SLACK_BOT_NAME = 'WorldCup Bot'
+const SLACK_BOT_NAME = process.env.SLACK_BOT_NAME
 
 const postMessage = async text => {
   const web = new WebClient(SLACK_TOKEN)
