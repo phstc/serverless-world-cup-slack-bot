2bead0ebedf49d412bded37fc799b6dea99be748
Pablo Cantero <pablohstc@gmail.com>
2018-06-29 10:37:25 -0400

    Organize tests

--- src/__test__/matches.test.js
+++ src/__test__/matches.test.js
@@ -2,15 +2,23 @@ const processMatches = require('../matches')
 
 jest.mock('../api')
 
-test('no new events', async () => {
-  const msgs = await processMatches()
+let msgs
 
+beforeAll(async () => {
+  msgs = await processMatches()
+})
+
+test('future matches', async () => {
   expect(msgs).toHaveLength(3)
   expect(msgs[0]).toEqual(
     ':timer_clock: The match between Brazil and Switzerland is about to start!'
   )
   // TODO figure out the date matcher
   expect(msgs[1]).toContain(':timer_clock: Brazil / Croatia is playing today')
+})
+
+test('completed matches', async () => {
+  expect(msgs).toHaveLength(3)
   expect(msgs[2]).toEqual(
     ':timer_clock: The match in between Brazil and Switzerland is over\n> Brazil 1 x Switzerland 1'
   )
